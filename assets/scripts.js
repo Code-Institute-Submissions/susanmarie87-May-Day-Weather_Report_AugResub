e7c4ec2bfa6d0ac33cff30a97932c006d06af7be
susanmarie87 <susan.marie87@gmail.com>
2021-05-04 17:20:59 +0000

    Added an event listener  to the seach box

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,3 +2,12 @@ const api = {
     key: "3868ebac95625426ec49b0031e0e6a5c",
     baseurl:"https://home.openweathermap.org/"
 }
+
+const searchbox = document.querySelector('search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function set(evt) {
+    if (evt.keyCode == 13 ) {
+        getResults(searchbox.value);
+    }
+}
