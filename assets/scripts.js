828c217418f0ea15f48396f48f1887481360c681
susanmarie87 <susan.marie87@gmail.com>
2021-05-05 03:27:36 +0000

    Added getResults function that will fetch api data

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,6 @@
 const api = {
     key: "3868ebac95625426ec49b0031e0e6a5c",
-    baseurl:"https://home.openweathermap.org/"
+    base:"https://home.openweathermap.org/"
 }
 
 const searchbox = document.querySelector('search-box');
@@ -10,4 +10,11 @@ function set(evt) {
     if (evt.keyCode == 13 ) {
         getResults(searchbox.value);
     }
+}
+
+function getResults (query) {
+    fetch (`${api.base}weather?q=${query}&units=imperial&APPID=${apikey}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
 }
