8a1fa3369aed30cc52cf985541205d14cf4a3b7e
susanmarie87 <susanmarie87@users.noreply.github.com>
2021-05-06 09:06:08 -0700

    Added days arrays to dateBuilder function

--- assets/scripts.js
+++ assets/scripts.js
@@ -32,5 +32,7 @@ function displayResults (weather){
 function dateBuilder (d) {
     let months= ["January", "February", "March", "April", "May", "June", "July"
     "August", "September", "October", "November", "December"];
-    
+    let days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday"
+    "Saturday"];
+
 }
