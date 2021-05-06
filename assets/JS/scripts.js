5d96045d18df6c37cded0afe4ef38ac3bedd1af6
susanmarie87 <susanmarie87@users.noreply.github.com>
2021-05-06 15:34:48 -0700

    Removed console.log

---
+++ assets/JS/scripts.js
@@ -0,0 +1,52 @@
+const api = {
+    key: "3868ebac95625426ec49b0031e0e6a5c",
+    base:"https://home.openweathermap.org/data/2.5"
+}
+
+const searchbox = document.querySelector('search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function set(evt) {
+    if (evt.keyCode == 13 ) {
+        getResults(searchbox.value);
+    }
+}
+
+function getResults (query) {
+    fetch (`${api.base}weather?q=${query}&units=imperial&APPID=${apikey}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather){
+    let city = document.querySelector
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');    
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
+ 
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innertext = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.inner = `${Math.round(weather.main.temp_min)}°f / ${Math.round(weather.main.temp_max)}°f`;
+}
+
+function dateBuilder (d) {
+    let months= ["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday",
+    "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+}
