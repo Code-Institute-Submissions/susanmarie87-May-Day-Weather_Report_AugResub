fbe671f04a26f635f076797163cd6084dc6e1305
susanmarie87 <susanmarie87@users.noreply.github.com>
2021-05-06 20:05:16 -0700

    Edited search-box to be .search-box

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -1,9 +1,9 @@
 const api = {
     key: "3868ebac95625426ec49b0031e0e6a5c",
-    base:"https://home.openweathermap.org/data/2.5"
+    base:"https://home.openweathermap.org/data/2.5/"
 }
 
-const searchbox = document.querySelector('search-box');
+const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
 function set(evt) {
