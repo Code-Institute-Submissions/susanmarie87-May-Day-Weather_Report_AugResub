c02ad6e463d2ce5cbebd19ae2b8e475b9c4a3630
susanmarie87 <susan.marie87@gmail.com>
2021-05-28 05:13:47 +0000

    Changed function layout/ format for readability

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,36 +1,13 @@
-const api = {
-    key: "926f9848e786e2fa78d6c0f226ee3e47",
-    base:"https://api.openweathermap.org/data/2.5/"
-}
-
-const searchbox = document.querySelector('.search-box');
-searchbox.addEventListener('keypress', setQuery);
-
-function setQuery(evt) {
-    if (evt.keyCode == 13 ) {
-        getWeatherData(searchbox.value);
-    }
-}
-
-function getWeatherData(cityName) {
-    fetch (`${api.base}weather?q=${cityName}&units=metric&APPID=${api.key}`)
-    .then(weather => {
-        return weather.json();
-    }).then(showWeatherResults);
-    
-}
-
 function showWeatherResults(weather) {
-    let city = document.querySelector('.location .city');
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
+    const locationCityRef = (document.querySelector('.location .city').innerText = `${weather.name}, ${weather.sys.country}`);
 
     let currentTime = new Date();
-    let date = document.querySelector('.location .date');    
+    let date = document.querySelector('.location .date');
     date.innerText = generateDate(currentTime);
 
     let temp = document.querySelector('.current .temp');
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
- 
+
     let currentWeather = document.querySelector('.current .weather');
     currentWeather.innertext = weather.weather[0].main;
 
@@ -38,11 +15,36 @@ function showWeatherResults(weather) {
     hilow.inner = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 }
 
+const api = {
+    key: "926f9848e786e2fa78d6c0f226ee3e47",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getWeatherData(searchbox.value);
+    }
+}
+
+function getWeatherData(cityName) {
+    fetch(`${api.base}weather?q=${cityName}&units=metric&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(showWeatherResults);
+
+}
+
+
 function generateDate(timeAndDate) {
-    let months= ["January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday",
-    "Saturday"];
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"
+    ];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+        "Saturday"
+    ];
 
     let day = days[timeAndDate.getDay()];
     let date = timeAndDate.getDate();
